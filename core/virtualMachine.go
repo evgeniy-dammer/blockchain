e00b5922a75403f922f885d62845425cebd84657
Evgeniy Dammer <evgeniydammer@gmail.com>
2023-07-14 12:24:24 +0500

    Add improvements for the virtual machine
    Add improvements for the virtual machine

--- core/virtualMachine.go
+++ core/virtualMachine.go
@@ -3,16 +3,47 @@ package core
 type Instruction byte
 
 const (
-	InstructionPush Instruction = 0x0a
-	instructionAdd  Instruction = 0x0b
+	InstructionPushInt  Instruction = 0x0a
+	instructionAdd      Instruction = 0x0b
+	InstructionPushByte Instruction = 0x0c
+	InstructionPack     Instruction = 0x0d
+	InstructionSub      Instruction = 0x0e
 )
+
+// Stack
+type Stack struct {
+	data         []any
+	stackPointer int
+}
+
+// NewStack is a constructor for the Stack
+func NewStack(size int) *Stack {
+	return &Stack{
+		data:         make([]any, size),
+		stackPointer: 0,
+	}
+}
+
+// Push pushes the given value at the end of the stack
+func (s *Stack) Push(value any) {
+	s.data[s.stackPointer] = value
+	s.stackPointer++
+}
+
+// Pop pops the value from the start of the stack
+func (s *Stack) Pop() any {
+	value := s.data[0]
+
+	s.data = append(s.data[:0], s.data[1:]...)
+	s.stackPointer--
+	return value
+}
 
 // VirtualMachine
 type VirtualMachine struct {
 	data               []byte
 	instructionPointer int
-	stack              []byte
-	stackPointer       int
+	stack              *Stack
 }
 
 // NewVirtualMachine is a constructor for the VirtualMachine
@@ -20,8 +51,7 @@ func NewVirtualMachine(data []byte) *VirtualMachine {
 	return &VirtualMachine{
 		data:               data,
 		instructionPointer: 0,
-		stack:              make([]byte, 1024),
-		stackPointer:       -1,
+		stack:              NewStack(128),
 	}
 }
 
@@ -47,21 +77,32 @@ func (vm *VirtualMachine) Run() error {
 // Exec executes the instruction
 func (vm *VirtualMachine) Exec(instruction Instruction) error {
 	switch instruction {
-	case InstructionPush:
-		vm.pushStack(vm.data[vm.instructionPointer-1])
+	case InstructionPushInt:
+		vm.stack.Push(int(vm.data[vm.instructionPointer-1]))
+	case InstructionPushByte:
+		vm.stack.Push(byte(vm.data[vm.instructionPointer-1]))
+	case InstructionPack:
+		n := vm.stack.Pop().(int)
+		b := make([]byte, n)
+
+		for i := 0; i < n; i++ {
+			b[i] = vm.stack.Pop().(byte)
+		}
+
+		vm.stack.Push(b)
+	case InstructionSub:
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
+		c := a - b
+
+		vm.stack.Push(c)
 	case instructionAdd:
-		a := vm.stack[0]
-		b := vm.stack[1]
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
 		c := a + b
 
-		vm.pushStack(c)
+		vm.stack.Push(c)
 	}
 
 	return nil
-}
-
-// pushStack pushes the instruction into the stack
-func (vm *VirtualMachine) pushStack(b byte) {
-	vm.stackPointer++
-	vm.stack[vm.stackPointer] = b
 }
--- core/virtualMachine_test.go
+++ core/virtualMachine_test.go
@@ -5,11 +5,35 @@ import (
 	"testing"
 )
 
+func TestStack_Pop(t *testing.T) {
+	stack := NewStack(128)
+
+	stack.Push(1)
+	stack.Push(2)
+
+	value := stack.Pop()
+
+	assert.Equal(t, value, 1)
+
+	value = stack.Pop()
+
+	assert.Equal(t, value, 2)
+}
+
+func TestStack_PushBytes(t *testing.T) {
+	s := NewStack(100)
+	s.Push(2)
+	s.Push(0x61)
+	s.Push(0x61)
+}
+
 func TestVirtualMachine_Run(t *testing.T) {
-	data := []byte{0x01, 0x0a, 0x02, 0x0a, 0x0b}
+	//data := []byte{0x02, 0x0a, 0x02, 0x0a, 0x0b}
+	data := []byte{0x03, 0x0a, 0x31, 0x0c, 0x40, 0x0c, 0x40, 0x0c, 0x0d}
 	virtualMachine := NewVirtualMachine(data)
 
 	assert.Nil(t, virtualMachine.Run())
 
-	assert.Equal(t, byte(3), virtualMachine.stack[virtualMachine.stackPointer])
+	//result := virtualMachine.stack.Pop()
+	//assert.Equal(t, 4, result)
 }
